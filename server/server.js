91857c808ca91ada55e6ecdd1a7d1d772e17a6f5
Traman2 <tejassraman@gmail.com>
2025-06-21 16:56:54 -0500

    added node server

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("MongoDB Connection failed", err));
+
+//Routes
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}...`);
+});
+
+export default app;
